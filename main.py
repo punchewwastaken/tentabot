22c78af080c7c016a8b80bc1e22e01936a03ed14
punchewwastaken <punchew.ew@gmail.com>
2025-08-20 11:43:21 +0200

    Added initial bot file stuff

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True  # Required for reading message content
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot.run('YOUR_BOT_TOKEN')
