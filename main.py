a531c45800fa1fcaf558707e771639ee92451418
punchewwastaken <punchew.ew@gmail.com>
2025-08-21 10:45:11 +0200

    added new tenta messages

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ with open('datum.json', 'r') as f:
 for each in datum:
     print(each)
 
+messages={
+    "tentamen":"Tentaregistration öppnar nu! Dags att registreta! Glöm inte att du även kan ansöka om plussning för att höja ditt betyg!",
+    "registration":"Kursregistreting har öppnat! Glöm inte att dubbelkolla vilka kurser du ska söka om!",
+    "anmalan":"Kursanmälan har öppnat på antagning.se! Glöm inte att ansöka till nästa termins kurser!",
+    "omtentamen":"Omtentaregistreting har öppnats! Om du behöver göra en omtenta, glöm inte att registrera dig!"
+}
+
 intents = discord.Intents.default()
 intents.message_content = True  # Required for reading message content
 
